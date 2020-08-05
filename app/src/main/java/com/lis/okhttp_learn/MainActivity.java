64a00241954a823d3c198775926109bb0238ccae
lis <xunyixiangchao@qq.com>
2020-08-05 22:23:16 +0800

    first commit

---
+++ app/src/main/java/com/lis/okhttp_learn/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lis.okhttp_learn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
